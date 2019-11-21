87c1fe31c6b1db7e56aae9e3e5ff37210ebd3584
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-11-21 14:04:13 -0500

    Update windows_startup.ps1

--- windows_startup.ps1
+++ windows_startup.ps1
@@ -1,1 +1,28 @@
-ECHO Hello world!
+Write-Host "Starting windows_startup.ps1"
+
+$cwd = Convert-Path .
+Write-Host "Current folder:" $cwd
+
+Write-Host "Parent folder:"
+cd ..
+dir
+
+$agentDir = "\agent"
+$provisionDir = "\provisioner"
+
+if (!(Test-Path -Path $agentDir))
+{
+   Write-Host "Creating agent folder"
+   New-Item -ItemType directory -Path $agentDir
+}
+else
+{
+   Write-Host "agent folder already exists"
+}
+
+cd $agentDir
+
+$cwd = Convert-Path .
+Write-Host "Current folder:" $cwd
+
+dir
