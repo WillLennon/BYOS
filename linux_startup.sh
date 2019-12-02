cf8933f10ae10e9ccca1dac0f74cdf365704d58a
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-12-02 11:30:50 -0500

    Update linux_startup.sh

--- linux_startup.sh
+++ linux_startup.sh
@@ -1,1 +1,22 @@
-ECHO Hello world!
+echo $1
+echo $2
+echo $3
+
+# We require 3 inputs: $1 is url, $2 is poolName, $3 is PAT
+url = $1
+poolName = $2
+pat = $3
+
+echo $url
+echo $poolName
+echo $pat
+
+ls
+
+mkdir -p /agent;
+tar zxvf  ~/vsts-agent-linux-x64-2.160.1.tar.gz --directory /agent;
+cd /agent;
+ls ./bin/
+./bin/installdependencies.sh;
+./config.sh --unattended --acceptTeeEula --url $url --pool $poolName --auth pat --token $pat && ./run.sh
+
