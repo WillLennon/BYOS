ba4bac6822d45f7b9eb09dd7ab47d1e96103df88
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-12-02 12:52:51 -0500

    Update linux_startup.sh

--- linux_startup.sh
+++ linux_startup.sh
@@ -7,14 +7,20 @@ url = $1
 poolName = $2
 pat = $3
 
-echo $url
-echo $poolName
-echo $pat
+echo url is $url
+echo pool name is $poolName
+echo pat is $pat
 
+pwd
 ls
 
 mkdir -p /agent;
-tar zxvf  ~/vsts-agent-linux-x64-2.160.1.tar.gz --directory /agent;
+
+zipfile=$(find vsts-agent*.tar.gz)
+
+echo the zip file is $zipfile
+
+tar -zxvf  $zipfile --directory /agent;
 cd /agent;
 ls ./bin/
 ./bin/installdependencies.sh;
