df775465fe8a8d16203254e728db7bcd9a43e615
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-12-02 14:13:03 -0500

    Update linux_startup.sh

--- linux_startup.sh
+++ linux_startup.sh
@@ -3,9 +3,9 @@ echo $2
 echo $3
 
 # We require 3 inputs: $1 is url, $2 is poolName, $3 is PAT
-url = $1
-poolName = $2
-pat = $3
+url=$1
+poolName=$2
+pat=$3
 
 echo url is $url
 echo pool name is $poolName
