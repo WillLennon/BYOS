d85d475d2a318dfec14d4a6839cffa5f53a81c4f
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-12-03 10:50:27 -0500

    Update linux_startup.sh

--- linux_startup.sh
+++ linux_startup.sh
@@ -1,10 +1,10 @@
 # We require 3 inputs: $1 is url, $2 is poolName, $3 is PAT
 url=$1
-poolName=$2
+pool=$2
 pat=$3
 
 echo url is $url
-echo pool name is $poolName
+echo pool is $pool
 echo pat is $pat
 
 pwd
@@ -24,8 +24,8 @@ pwd
 echo installing dependencies
 ./bin/installdependencies.sh
 
-echo configuring build agent
-sh ./config.sh --unattended --acceptTeeEula --url $url --pool $poolName --auth pat --token $pat &
+echo configuring build agent $url $pool $pat
+./config.sh --unattended --acceptTeeEula --url $url --pool $pool --auth pat --token $pat
 
 pwd
 ls -a
@@ -33,4 +33,4 @@ ls -a
 #echo running build agent
 #sh ./run.sh
 
-echo done?
+echo done
