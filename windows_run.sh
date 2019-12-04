25c3e8476a3f73c7d7ac814e14e5931fe071b7b3
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-12-04 14:51:36 -0500

    Update windows_run.sh

--- windows_run.sh
+++ windows_run.sh
@@ -29,25 +29,8 @@ Write-Host "PAT: " $pat
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $agentDir = "\agent"
-$agentExe = Join-Path -Path $agentDir -ChildPath "bin\Agent.Listener.exe"
-$agentZip = Get-ChildItem -Path .\* -File -Include vsts-agent*.zip
 $agentConfig = Join-Path -Path $agentDir -ChildPath "config.cmd"
 $agentRun = Join-Path -Path $agentDir -ChildPath "run.cmd"
-
-#
-# install the build agent if necessary
-#
-if (!(Test-Path -Path $agentDir))
-{
-   Write-Host "Creating agent folder"
-   New-Item -ItemType directory -Path $agentDir
-}
-
-if (!(Test-Path -Path $agentExe))
-{
-   Write-Host "Unzipping agent"
-   [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentDir)
-}
 
 # configure the build agent
 $configParameters = " --unattended --url $url --runAsAutoLogon --noRestart  --pool $pool --auth pat --token $pat"
