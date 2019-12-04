43b71fd35a9916ff5a7e58d14f1d4d89da8f7583
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2019-12-04 14:49:33 -0500

    Update windows_setup.ps1

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -1,38 +1,8 @@
-param
-(
-   [string]$url,
-   [string]$pool,
-   [string]$pat
-)
-
-$errorActionPreference = 'Stop'
-
-if ([string]::IsNullOrEmpty($url))
-{
-   Write-Error "URL is null"
-}
-
-if ([string]::IsNullOrEmpty($pool))
-{
-   Write-Error "Pool is null"
-}
-
-if ([string]::IsNullOrEmpty($pat))
-{
-   Write-Error "PAT is null"
-}
-
-Write-Host "URL: " $url
-Write-Host "Pool:" $pool
-Write-Host "PAT: " $pat
-
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $agentDir = "\agent"
 $agentExe = Join-Path -Path $agentDir -ChildPath "bin\Agent.Listener.exe"
 $agentZip = Get-ChildItem -Path .\* -File -Include vsts-agent*.zip
-$agentConfig = Join-Path -Path $agentDir -ChildPath "config.cmd"
-$agentRun = Join-Path -Path $agentDir -ChildPath "run.cmd"
 
 #
 # install the build agent if necessary
@@ -49,14 +19,4 @@ if (!(Test-Path -Path $agentExe))
    [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentDir)
 }
 
-# configure the build agent
-$configParameters = " --unattended --url $url --runAsAutoLogon --noRestart  --pool $pool --auth pat --token $pat"
-$config = $agentConfig + $configParameters
-Write-Host "Running " $config
-Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
-
-# run the build agent
-Write-Host "Running " $agentRun
-Start-Process $agentRun -NoNewWindow -WorkingDirectory $agentDir
-
-Write-Host "Done"
+Write-Host "Done unzipping agent"
