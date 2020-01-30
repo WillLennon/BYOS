71617f5cf89dcd7aa2c28a34956aceb2ae55c93e
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2020-01-30 15:33:04 -0500

    Update windows_run_agent.ps1

--- windows_run_agent.ps1
+++ windows_run_agent.ps1
@@ -1,42 +1,5 @@
-param
-(
-   [string]$url,
-   [string]$pool,
-   [string]$pat
-)
-
-$errorActionPreference = 'Stop'
-
-if ([string]::IsNullOrEmpty($url))
-{
-   Write-Error "URL is null"
-}
-
-if ([string]::IsNullOrEmpty($pool))
-{
-   Write-Error "Pool is null"
-}
-
-if ([string]::IsNullOrEmpty($pat))
-{
-   Write-Error "PAT is null"
-}
-
-Write-Host "URL: " $url
-Write-Host "Pool:" $pool
-Write-Host "PAT: " $pat
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-
 $agentDir = "\agent"
-$agentConfig = Join-Path -Path $agentDir -ChildPath "config.cmd"
 $agentRun = Join-Path -Path $agentDir -ChildPath "run.cmd"
-
-# configure the build agent
-$configParameters = " --unattended --url $url --runAsAutoLogon --noRestart  --pool $pool --auth pat --token $pat"
-$config = $agentConfig + $configParameters
-Write-Host "Running " $config
-Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
 
 # run the build agent
 Write-Host "Running " $agentRun
