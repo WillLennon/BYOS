bde7e0d62db424ccb356f93d3387535e5213f286
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2020-01-30 20:45:19 -0500

    Update windows_run_agent.ps1

--- windows_run_agent.ps1
+++ windows_run_agent.ps1
@@ -1,9 +1,4 @@
-# Schedule a task to start the build agent in 10 seconds.
-# We cannot start it ourselves because Azure ScaleSets will keep the VM stuck in the Updating state until it times out several hours later.
-
-$agentDir = "\agent"
-$agentRun = Join-Path -Path $agentDir -ChildPath "run.cmd"
-$start= (Get-Date).AddSeconds(10)
+$start = (Get-Date).AddSeconds(2)
 $time = New-ScheduledTaskTrigger -At $start -Once 
-$cmd = New-ScheduledTaskAction -Execute $agentRun -WorkingDirectory $agentDir
-Register-ScheduledTask -TaskName "BuildAgent" -Trigger $time -Action $cmd -TaskPath $agentDir
+$cmd = New-ScheduledTaskAction -Execute \agent\run.cmd -WorkingDirectory \agent
+Register-ScheduledTask -TaskName "BuildAgent" -Trigger $time -Action $cmd -TaskPath \agent -Force
