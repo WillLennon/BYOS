091a475aeb67427d13fb4273b153388f6ce330c2
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2020-01-31 07:47:28 -0500

    Update windows_run_agent.ps1

--- windows_run_agent.ps1
+++ windows_run_agent.ps1
@@ -1,4 +1,4 @@
 $start = (Get-Date).AddSeconds(2)
 $time = New-ScheduledTaskTrigger -At $start -Once 
-$cmd = New-ScheduledTaskAction -Execute \agent\run.cmd -WorkingDirectory \agent
-Register-ScheduledTask -TaskName "BuildAgent" -Trigger $time -Action $cmd -TaskPath \agent -Force
+$cmd = New-ScheduledTaskAction -Execute c:\agent\run.cmd -WorkingDirectory c:\agent
+Register-ScheduledTask -TaskName "BuildAgent" -Trigger $time -Action $cmd -TaskPath c:\agent -Force
