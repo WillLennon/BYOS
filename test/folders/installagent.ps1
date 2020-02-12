e4a1f11f92707c67a964dd8db0d900088ad64bb5
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2020-02-12 08:17:43 -0500

    Create installagent.ps1

---
+++ test/folders/installagent.ps1
@@ -0,0 +1,62 @@
+param
+(
+   [string]$url,
+   [string]$pool,
+   [string]$pat
+)
+
+$errorActionPreference = 'Stop'
+
+if ([string]::IsNullOrEmpty($url))
+{
+   Write-Error "URL is null"
+}
+
+if ([string]::IsNullOrEmpty($pool))
+{
+   Write-Error "Pool is null"
+}
+
+if ([string]::IsNullOrEmpty($pat))
+{
+   Write-Error "PAT is null"
+}
+
+Write-Host "URL: " $url
+Write-Host "Pool:" $pool
+Write-Host "PAT: " $pat
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$agentDir = "\agent"
+$agentExe = Join-Path -Path $agentDir -ChildPath "bin\Agent.Listener.exe"
+$agentZip = Get-ChildItem -Path .\* -File -Include vsts-agent*.zip
+$agentConfig = Join-Path -Path $agentDir -ChildPath "config.cmd"
+$agentRun = Join-Path -Path $agentDir -ChildPath "run.cmd"
+
+# install the build agent if necessary
+if (!(Test-Path -Path $agentDir))
+{
+   Write-Host "Creating agent folder"
+   New-Item -ItemType directory -Path $agentDir
+}
+
+# copy run script to the agent folder
+$runScript = "runagent.ps1"
+$runPath = Join-Path -Path $agentDir -ChildPath $runScript
+Copy-item $runScript $runPath
+
+if (!(Test-Path -Path $agentExe))
+{
+   Write-Host "Unzipping agent"
+   [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentDir)
+}
+
+# configure the build agent
+$configParameters = " --unattended --url $url --noRestart  --pool ""$pool"" --auth pat --token $pat"
+$config = $agentConfig + $configParameters
+Write-Host "Running " $config
+Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
+
+# schedule the build agent to run immediately
+Start-Process -FilePath Powershell.exe -ArgumentList "-ExecutionPolicy Unrestricted $runPath" -NoNewWindow -Wait
