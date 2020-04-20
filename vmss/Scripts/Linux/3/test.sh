ba8d1a39d4813bc03955865a50cb8d012532dc02
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2020-04-20 12:23:49 -0400

    Create test.sh

---
+++ vmss/Scripts/Linux/3/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# We require 3 inputs: $1 is url, $2 is pool, $3 is PAT
+# 4th input is option $4 is either '--once' or null
+url=$1
+pool=$2
+pat=$3
+runArgs=$4
+
+echo url $url
+echo pool $pool
+echo pat $pat
+
+# Create our user account
+echo creating AzDevOps account
+sudo useradd -m AzDevOps
+sudo usermod -a -G sudo AzDevOps
+
+# Create agent folder
+echo creating agent folder
+mkdir -p -v /agent
+
+# Copy run script
+cp runagent.sh /agent/runagent.sh
+
+echo url $url
+echo pool $pool
+echo pat $pat
+
+# configure the build agent
+# calling bash here so the quotation marks around $pool get respected
+echo configuring build agent
+sudo runuser AzDevOps -c '/bin/bash ./config.sh --unattended --url $url --pool "$pool" --auth pat --token $pat --acceptTeeEula --replace'
+
+# schedule the build agent to run immediately
+/bin/bash ./runagent.sh $runArgs
+
+echo done
