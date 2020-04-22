d0b8a980c6fc1bf29198b43782e3d02976b50685
Will Lennon <39530873+WillLennon@users.noreply.github.com>
2020-04-22 15:14:45 -0400

    Update test.sh

--- vmss/Scripts/Linux/3/test.sh
+++ vmss/Scripts/Linux/3/test.sh
@@ -7,26 +7,7 @@ pool=$2
 pat=$3
 runArgs=$4
 
-echo url $url
-echo pool $pool
-echo pat $pat
-
-# Copy run script
-cp runagent.sh /agent/runagent.sh
-
-cd /agent
-
-echo url $url
-echo url2 '$url'
-echo url3 "$url"
-
-echo pool $pool
-echo pat $pat
-
-# configure the build agent
-# calling bash here so the quotation marks around $pool get respected
-echo configuring build agent
-sudo runuser AzDevOps -c "/bin/bash ./config.sh --unattended --url $url --pool '$pool' --auth pat --token $pat --acceptTeeEula --replace"
+echo $runArgs
 
 # schedule the build agent to run immediately
 /bin/bash ./runagent.sh $runArgs
