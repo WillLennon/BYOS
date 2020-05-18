4fda21f1743bf4d7e8d33a145804e03d58c81567
Will Lennon <willlennon@github.com>
2020-05-18 09:20:59 -0400

    Create runagent.ps1

---
+++ vmss/Scripts/Linux/4/runagent.ps1
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#delete the script folder
+#rm -rf "/var/lib/waagent/custom-script"
+
+#pass any input argument to the run script
+echo "sudo runuser AzDevOps -c \"/bin/bash /agent/run.sh $1\"" | at now
