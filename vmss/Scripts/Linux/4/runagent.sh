42bb279f8a9bdf74017af6cb2ef61106f8a0a65f
Will Lennon <willlennon@github.com>
2020-05-18 11:18:30 -0400

    Update runagent.sh

--- vmss/Scripts/Linux/4/runagent.sh
+++ vmss/Scripts/Linux/4/runagent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -rf "/var/lib/waagent/custom-script"
+# TESTING ONLY
+#rm -rf "/var/lib/waagent/custom-script"
 
 #pass any input argument to the run script
 echo "sudo runuser AzDevOps -c \"/bin/bash /agent/run.sh $1\"" | at now
