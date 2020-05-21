92d0e4ee9ca21605d5dc220fd1db94667b893e29
Will Lennon <willlennon@github.com>
2020-05-21 18:34:55 -0400

    Update installagent.ps1

--- vmss/Scripts/Windows/4/installagent.ps1
+++ vmss/Scripts/Windows/4/installagent.ps1
@@ -51,6 +51,19 @@ if ($windows.Edition -like '*datacenter*' -or
   net localgroup docker-users $username /add
 }
 
+# disable UAC so the warmup script doesn't prompt when we elevate
+$osversion = (Get-CimInstance Win32_OperatingSystem).Version 
+$version = $osversion.split(".")[0] 
+ 
+if ($version -eq 10) 
+{ 
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0" 
+} 
+else
+{ 
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0" 
+}
+
 # run the customer warmup script if it exists
 $warmup = "\warmup.ps1"
 if (Test-Path -Path $warmup)
@@ -62,13 +75,13 @@ if (Test-Path -Path $warmup)
       # run as local admin
       $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
       $credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
-      Start-Process -FilePath PowerShell.exe -ArgumentList $warmup -Verb RunAs -NoNewWindow -Wait -WorkingDirectory "\" -Credential $credential
+      Start-Process powershell.exe -Credential $credential -Wait -WorkingDirectory "\" -ArgumentList "start-process powershell.exe $warmup -verb runas"
    }
    else
    {
       # run as system
       echo runassystem > runassystem.txt
-      Start-Process -FilePath $warmup -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory "\"
+      Start-Process powershell.exe -Wait -WorkingDirectory "\" -ArgumentList $warmup -Verb runas
    }
 }
 
