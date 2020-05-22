b45f2c72f7469222e38be9090f0f3838b6875f0c
Will Lennon <willlennon@github.com>
2020-05-22 17:13:19 -0400

    Update runagent.ps1

--- vmss/Scripts/Windows/4/runagent.ps1
+++ vmss/Scripts/Windows/4/runagent.ps1
@@ -31,14 +31,29 @@ Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindo
 $start1 = (Get-Date).AddSeconds(15)
 $time1 = New-ScheduledTaskTrigger -At $start1 -Once 
 if([string]::IsNullOrEmpty($runArgs))
-{  $cmd1 = New-ScheduledTaskAction -Execute "C:\agent\run.cmd" -WorkingDirectory "C:\agent" }
+{
+  $cmd1 = New-ScheduledTaskAction -Execute "C:\agent\run.cmd" -WorkingDirectory "C:\agent"
+}
 else
-{  $cmd1 = New-ScheduledTaskAction -Execute "C:\agent\run.cmd" -WorkingDirectory "C:\agent" $runArgs }
+{
+  $cmd1 = New-ScheduledTaskAction -Execute "C:\agent\run.cmd" -WorkingDirectory "C:\agent" -Argument $runArgs
+}
 
-Register-ScheduledTask -TaskName "BuildAgent" -Trigger $time1 -Action $cmd1 -Force
+$windows = Get-WindowsEdition -Online
+
+if ($windows.Edition -like '*datacenter*' -or
+    $windows.Edition -like '*server*' )
+{
+  Register-ScheduledTask -TaskName "BuildAgent" -User $username -Password $password -Trigger $time1 -Action $cmd1 -Force
+}
+else
+{
+  Register-ScheduledTask -TaskName "BuildAgent" -User System -Trigger $time1 -Action $cmd1 -Force
+}
 
 # delete the status folder from custom script extension directory
 $start2 = (Get-Date).AddSeconds(15)
 $time2 = New-ScheduledTaskTrigger -At $start2 -Once 
 $cmd2 = New-ScheduledTaskAction -Execute Powershell.exe -Argument 'Remove-Item "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\*\Status" -Recurse -ErrorAction Ignore'
 Register-ScheduledTask -TaskName "ExtensionCleanup" -User System -Trigger $time2 -Action $cmd2 -Force
+
