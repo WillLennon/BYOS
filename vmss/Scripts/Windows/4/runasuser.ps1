fa554b0f5a1cfcd38697f16ff842b377223a567e
Will Lennon <willlennon@github.com>
2020-05-22 16:08:58 -0400

    Update runasuser.ps1

--- vmss/Scripts/Windows/4/runasuser.ps1
+++ vmss/Scripts/Windows/4/runasuser.ps1
@@ -16,12 +16,9 @@ if (Test-Path -Path $warmup)
    {
       $now = Get-Date
       echo $now > c:\start.txt
-      # run as local admin
 
-      # This is wonky.  
-      # We want to run powershell both elevated and as the local admin, but Powershell won't let you do both -Credential and -Verb.
-      # So start a process as the local admin and then have that process start another elevated process that runs the warmup script.
-      Start-Process -FilePath PowerShell.exe -Credential $credential -Wait -ArgumentList $warmup
+      # run as local admin elevated
+      Start-Process -FilePath PowerShell.exe -Verb RunAs -Wait -ArgumentList $warmup
       $now = Get-Date
       echo $now > c:\finish.txt
    }
@@ -37,8 +34,7 @@ if (Test-Path -Path $warmup)
 $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $pat"
 $config = $agentConfig + $configParameters
 Write-Host "Running " $config
-Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
+Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir -Verb RunAs
 
 # schedule the build agent to run
-Start-Process -FilePath Powershell.exe -ArgumentList "-ExecutionPolicy Unrestricted $runFileDest $runArgs $username $password"
-
+Start-Process -FilePath Powershell.exe -ArgumentList "-ExecutionPolicy Unrestricted $runFileDest $runArgs"
