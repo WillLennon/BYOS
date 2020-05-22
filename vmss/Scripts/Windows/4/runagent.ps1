27c28f792a2b1b61ea474242120589dd58b08ca2
Will Lennon <willlennon@github.com>
2020-05-22 16:12:24 -0400

    Update runagent.ps1

--- vmss/Scripts/Windows/4/runagent.ps1
+++ vmss/Scripts/Windows/4/runagent.ps1
@@ -1,9 +1,38 @@
 param
 (
-   [string]$runArgs,
+   [string]$runArgs
 )
 
-# v4
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+# run the customer warmup script if it exists
+$warmup = "\warmup.ps1"
+if (Test-Path -Path $warmup)
+{
+   if (![String]::IsNullOrEmpty($username))
+   {
+      $now = Get-Date
+      echo $now > c:\start.txt
+
+      # run as local admin elevated
+      Start-Process -FilePath PowerShell.exe -Verb RunAs -Wait -ArgumentList $warmup
+      $now = Get-Date
+      echo $now > c:\finish.txt
+   }
+   else
+   {
+      # run as system
+      echo runassystem > c:\runassystem.txt
+      Start-Process -FilePath PowerShell.exe -Wait -ArgumentList $warmup -WorkingDirectory '\' -Verb RunAs
+   }
+}
+
+# configure the build agent
+$configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $pat"
+$config = $agentConfig + $configParameters
+Write-Host "Running " $config
+Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir -Verb RunAs
+
 # schedule the build agent to run
 $start1 = (Get-Date).AddSeconds(15)
 $time1 = New-ScheduledTaskTrigger -At $start1 -Once 
