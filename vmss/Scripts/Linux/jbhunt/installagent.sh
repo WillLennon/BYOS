2ca40c9d1f67859d86ca2b3816363a3471634787
Will Lennon <willlennon@github.com>
2020-06-02 07:39:23 -0400

    Update installagent.sh

--- vmss/Scripts/Linux/jbhunt/installagent.sh
+++ vmss/Scripts/Linux/jbhunt/installagent.sh
@@ -32,6 +32,8 @@ echo 'AzDevOps ALL=NOPASSWD: ALL' >> /etc/sudoers
 
 # Copy run script
 cp runagent.sh /agent/runagent.sh
+chmod 777 /agent/runagent.sh
+
 # TEST ONLY
 cp installagent.sh /agent/installagent.sh
 
