d366f86e1c16bb5064bdc389a3248b4173208edd
Will Lennon <willlennon@github.com>
2020-06-03 10:20:12 -0400

    Update enableext.ps1

--- vmss/Scripts/Windows/5/enableext.ps1
+++ vmss/Scripts/Windows/5/enableext.ps1
@@ -3,13 +3,40 @@ param
    [string]$runArgs
 )
 
+function Log-Message 
+{
+   param ([string] $message)
+
+   $now = [DateTime]::UtcNow.ToString('u')
+   $text = $now + " " + $message
+   Add-Content -Path status.txt -Value $text
+}
+
+
+Log-Message "Enabling Extension"
+Log-Message "runArgs: " + $runArgs
+
 $username = Get-Content username.txt
 $password = Get-Content password.txt
+
+if([string]::IsNullOrEmpty($username))
+{
+   Log-Message "username not found"
+   exit
+}
+
+if([string]::IsNullOrEmpty($password))
+{
+   Log-Message "password not found"
+   exit
+}
 
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
 $runCmd = Join-Path -Path $PSScriptRoot -ChildPath "run.cmd"
+
+Log-Message $runCmd
 
 if([string]::IsNullOrEmpty($runArgs))
 {
@@ -20,4 +47,6 @@ else
    Start-Process -FilePath $runCmd -Credential $credential -ArgumentList $runArgs
 }
 
+Log-Message "Deleting password"
 Remove-Item password.txt
+Log-Message "Finished"
