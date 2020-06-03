e1dd90d068ffb0873da6b8d47ed0184479746bc7
Will Lennon <willlennon@github.com>
2020-06-03 12:02:40 -0400

    Update installext.ps1

--- vmss/Scripts/Windows/5/installext.ps1
+++ vmss/Scripts/Windows/5/installext.ps1
@@ -11,7 +11,8 @@ function Log-Message
 
    $now = [DateTime]::UtcNow.ToString('u')
    $text = $now + " " + $message
-   Add-Content -Path status.txt -Value $text
+   $logfile = Join-Path -Path $PSScriptRoot -ChildPath "status.txt"
+   Add-Content -Path $logfile -Value $text
 }
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
