55116225eb0016ccfb99670fc3605abd021b50d6
Will Lennon <willlennon@github.com>
2020-06-03 10:25:21 -0400

    Update installext.ps1

--- vmss/Scripts/Windows/5/installext.ps1
+++ vmss/Scripts/Windows/5/installext.ps1
@@ -5,11 +5,24 @@ param
    [string]$token
 )
 
+function Log-Message 
+{
+   param ([string] $message)
+
+   $now = [DateTime]::UtcNow.ToString('u')
+   $text = $now + " " + $message
+   Add-Content -Path status.txt -Value $text
+}
+
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-Set-Content -Path status.txt -Value "Installing extension"
-Add-Content -Path status.txt -Value $url
-Add-Content -Path status.txt -Value $pool 
+Set-Content -Path status.txt -Value ""
+Log-Message "Installing extension"
+$url
+$pool
+
+Log-Message ("URL: " + $url)
+Log-Message ("Pool: " + $pool) 
 
 $agentDir = $PSScriptRoot
 $agentExe = Join-Path -Path $agentDir -ChildPath "bin\Agent.Listener.exe"
@@ -19,12 +32,12 @@ $agentConfig = Join-Path -Path $agentDir -ChildPath "config.cmd"
 #unzip the agent if it doesn't exist already
 if (!(Test-Path -Path $agentExe))
 {
-   Add-Content -Path status.txt -Value "Unzipping Agent"
+   Log-Message "Unzipping Agent"
    [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentDir)
 }
 
 # create administrator account
-Add-Content -Path status.txt -Value "Creating AzDevOps account"
+Log-Message  "Creating AzDevOps account"
 $username = 'AzDevOps'
 Set-Content -Path username.txt -Value $username
 $password = (New-Guid).ToString()
@@ -33,30 +46,30 @@ $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 
 if (!(Get-LocalUser -Name $username -ErrorAction Ignore))
 {
-  Add-Content -Path status.txt -Value "Creating AzDevOps user"
+  Log-Message "Creating AzDevOps user"
   New-LocalUser -Name $username -Password $securePassword
 }
 else
 {
-  Add-Content -Path status.txt -Value "Setting AzDevOps password"
+  Log-Message "Setting AzDevOps password"
   Set-LocalUser -Name $username -Password $securePassword 
 }
 if ((Get-LocalGroup -Name "Users" -ErrorAction Ignore) -and
     !(Get-LocalGroupMember -Group "Users" -Member $username -ErrorAction Ignore))
 {
-  Add-Content -Path status.txt -Value "Adding AzDevOps to Users"
+  Log-Message "Adding AzDevOps to Users"
   Add-LocalGroupMember -Group "Users" -Member $username
 }
 if ((Get-LocalGroup -Name "Administrators" -ErrorAction Ignore) -and
     !(Get-LocalGroupMember -Group "Administrators" -Member $username -ErrorAction Ignore))
 {
-  Add-Content -Path status.txt -Value "Adding AzDevOps to Administrators"
+  Log-Message "Adding AzDevOps to Administrators"
   Add-LocalGroupMember -Group "Administrators" -Member $username
 }
 if ((Get-LocalGroup -Name "docker-users" -ErrorAction Ignore) -and
     !(Get-LocalGroupMember -Group "docker-users" -Member $username -ErrorAction Ignore))
 {
-  Add-Content -Path status.txt -Value "Adding AzDevOps to docker-users"
+  Log-Message "Adding AzDevOps to docker-users"
   Add-LocalGroupMember -Group "docker-users" -Member $username
 }
 
@@ -66,13 +79,13 @@ $warmup = "\warmup.ps1"
 if (Test-Path -Path $warmup)
 {
    # run as local admin elevated
-   Add-Content -Path status.txt -Value "Running warmup script"
+   Log-Message "Running warmup script"
    Start-Process -FilePath PowerShell.exe -Verb RunAs -Wait -WorkingDirectory \ -ArgumentList "-ExecutionPolicy Unrestricted $warmup"
 }
 
 # configure the build agent
 $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $token"
 $config = $agentConfig + $configParameters
-Add-Content -Path status.txt -Value "Configuring agent"
+Log-Message "Configuring agent"
 Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
-Add-Content -Path status.txt -Value "Finished configuration."
+Log-Message "Finished configuration."
