4c096d9a680c81472be8c86168e3ecf27eccb80b
Will Lennon <willlennon@github.com>
2020-06-15 13:07:15 -0400

    Update enableagent.ps1

--- vmss/Scripts/Windows/4/enableagent.ps1
+++ vmss/Scripts/Windows/4/enableagent.ps1
@@ -54,6 +54,11 @@ if (!(Test-Path -Path $agentExe))
    }
 }
 
+# delete old configuration files if present
+Remove-Item -Path (Join-Path -Path $agentDir -ChildPath ".agent") -Force -ErrorAction Ignore
+Remove-Item -Path (Join-Path -Path $agentDir -ChildPath ".credentials") -Force -ErrorAction Ignore
+Remove-Item -Path (Join-Path -Path $agentDir -ChildPath ".credentials_rsaparams") -Force -ErrorAction Ignore
+
 # create administrator account
 Log-Message  "Creating AzDevOps account"
 $username = 'AzDevOps'
