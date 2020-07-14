29415bfa7c60c05efa01b3a8369c490d048d6738
Will Lennon <willlennon@github.com>
2020-07-14 16:30:03 -0400

    Update test.ps1

--- test/CSE/test.ps1
+++ test/CSE/test.ps1
@@ -1,1 +1,41 @@
-#powershell custom script extension script
+param
+(
+   [int]$sleep,
+   [int]$exitCode,
+   [switch]$throw,
+)
+
+function Log-Message 
+{
+   param ([string] $message)
+
+   $now = [DateTime]::UtcNow.ToString('u')
+   $text = $now + " " + $message
+   $logFile = "c:\script.log"
+   if (!(Test-Path -Path $logFile))
+   {
+      Set-Content -Path $logFile -Value ""
+   }
+   Add-Content -Path $logFile -Value $text
+   Write-Host $text
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+Log-Message "Test CSE"
+Log-Message ("sleep: " + $sleep)
+Log-Message ("exitCode: " + $exitCode)
+Log-Message ("throw: " + $throw) 
+
+Log-Message "sleeping"
+Start-Sleep $sleep
+
+# If the agent was already configured.  Abort.
+if ($throw)
+{
+   Log-Message "throwing"
+   throw [System.Exception] "throwing exception"
+}
+
+Log-Message "exiting"
+exit $exitCode
