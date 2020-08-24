7fd66d6aba476ae628e53167e93a999ed01c6e7e
Will Lennon <willlennon@github.com>
2020-08-24 15:41:10 -0400

    reboot twice

--- vmss/Scripts/Windows/8/enableagent.ps1
+++ vmss/Scripts/Windows/8/enableagent.ps1
@@ -58,177 +58,212 @@ if (Test-Path -Path (Join-Path -Path $agentDir -ChildPath ".agent"))
    exit 0
 }
 
-# Disable Windows Updates so the machine won't spontaneously reboot
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name AUOptions -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoRebootWithLoggedOnUsers -Value 0
+# Check if this is before or after reboot
+$username = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName
+$password = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword
 
-# set the agent working directory to 'C:\a' if the environment variable is not set already
-$workDir = [System.Environment]::GetEnvironmentVariable('VSTS_AGENT_INPUT_WORK')
-if (![string]::IsNullOrEmpty($workDir))
+if ([string]::IsNullOrEmpty($username) -or [string]::IsNullOrEmpty($password))
 {
-    Log-Message ("Found WorkDir: " + $workDir)
-}
-else
-{
-    $drive = (Get-Location).Drive.Name + ":"
-    $workDir = Join-Path -Path $drive -ChildPath "a"
-    [System.Environment]::SetEnvironmentVariable('VSTS_AGENT_INPUT_WORK', $workDir, 'process')
-    [System.Environment]::SetEnvironmentVariable('VSTS_AGENT_INPUT_WORK', $workDir, 'machine')
-    Log-Message ("Setting WorkDir: " + $workDir)
-}
+    Log-Message "Registry keys not found"
 
-# unzip the agent if it doesn't exist already
-if (!(Test-Path -Path $agentExe))
-{
-   Log-Message "Unzipping Agent"
-   try
-   {
-      [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentDir)
-      Remove-Item $agentZip
-   }
-   catch
-   {
-      Log-Message $Error[0]
-      exit -100
-   }
-}
+    Log-Message "Disabling Windows Update"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -Value 1
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name AUOptions -Value 1
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoRebootWithLoggedOnUsers -Value 0
 
-if ($runAsUser)
-{
-   # create administrator account
-   $username = 'AzDevOps'
-   $password = 'A1b2838!K#n0sn0abc' #(New-Guid).ToString()
-   $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+    # set the agent working directory to 'C:\a' if the environment variable is not set already
+    $workDir = [System.Environment]::GetEnvironmentVariable('VSTS_AGENT_INPUT_WORK')
+    if (![string]::IsNullOrEmpty($workDir))
+    {
+        Log-Message ("Found WorkDir: " + $workDir)
+    }
+    else
+    {
+        $drive = (Get-Location).Drive.Name + ":"
+        $workDir = Join-Path -Path $drive -ChildPath "a"
+        [System.Environment]::SetEnvironmentVariable('VSTS_AGENT_INPUT_WORK', $workDir, 'process')
+        [System.Environment]::SetEnvironmentVariable('VSTS_AGENT_INPUT_WORK', $workDir, 'machine')
+        Log-Message ("Setting WorkDir: " + $workDir)
+    }
 
-   if (!(Get-LocalUser -Name $username -ErrorAction Ignore))
-   {
-      Log-Message "Creating AzDevOps user"
-      New-LocalUser -Name $username -Password $securePassword
-   }
-   else
-   {
-      Log-Message "Setting AzDevOps password"
-      Set-LocalUser -Name $username -Password $securePassword 
-   }
-   Log-Message ("password: " + $password)
-
-   # Confirm the local user exists or abort if not
-   if (!(Get-LocalUser -Name $username))
-   {
-      Log-Message "Failed to create AzDevOps user"
-      exit -105
-   }
-
-   if ((Get-LocalGroup -Name "Users" -ErrorAction Ignore) -and
-       !(Get-LocalGroupMember -Group "Users" -Member $username -ErrorAction Ignore))
-   {
-      Log-Message "Adding AzDevOps to Users"
-      Add-LocalGroupMember -Group "Users" -Member $username
-   }
-   if ((Get-LocalGroup -Name "Administrators" -ErrorAction Ignore) -and
-       !(Get-LocalGroupMember -Group "Administrators" -Member $username -ErrorAction Ignore))
-   {
-      Log-Message "Adding AzDevOps to Administrators"
-      Add-LocalGroupMember -Group "Administrators" -Member $username
-   }
-   if ((Get-LocalGroup -Name "docker-users" -ErrorAction Ignore) -and
-       !(Get-LocalGroupMember -Group "docker-users" -Member $username -ErrorAction Ignore))
-   {
-      Log-Message "Adding AzDevOps to docker-users"
-      Add-LocalGroupMember -Group "docker-users" -Member $username
-   }
-
-   if ($interactive)
-   {
-      Log-Message "Configuring agent to reboot, autologon, and run unelevated as AzDevOps with interactive UI"
-      $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --replace --runAsAutoLogon --overwriteAutoLogon --windowsLogonAccount $username --windowsLogonPassword $password --token $token"
-      try
-      {
-         Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
-      }
-      catch
-      {
-         Log-Message $Error[0]
-         exit -102
-      }
-   }
-   else
-   {
-      Log-Message "Configuring agent to run elevated as AzDevOps without interactive UI"
-      $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $token"
-      try
-      {
-         Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
-      }
-      catch
-      {
-         Log-Message $Error[0]
-         exit -106
-      }
-
-      Log-Message "Scheduling agent to run"
-      $runCmd = Join-Path -Path $agentDir -ChildPath "run.cmd"
-      try
-      {
-         if([string]::IsNullOrEmpty($runArgs))
-         {
-            $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir
-         }
-         else
-         {
-            $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir $runArgs
-         }
-
-         $start1 = (Get-Date).AddSeconds(10)
-         $time1 = New-ScheduledTaskTrigger -At $start1 -Once 
-         Register-ScheduledTask -TaskName "PipelinesAgent" -User $username -Password $password -RunLevel Highest -Trigger $time1 -Action $cmd1 -Force
-      }
-      catch
-      {
+    # unzip the agent if it doesn't exist already
+    if (!(Test-Path -Path $agentExe))
+    {
+       Log-Message "Unzipping Agent"
+       try
+       {
+          [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentDir)
+          Remove-Item $agentZip
+       }
+       catch
+       {
           Log-Message $Error[0]
-          exit -108
-      }
-   }
+          exit -100
+       }
+    }
+
+    if ($runAsUser)
+    {
+        # create administrator account
+        $username = 'AzDevOps'
+        $password = 'A1b2838!K#n0sn0abc' #(New-Guid).ToString()                   TODO FIX THIS!
+        $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+
+       if (!(Get-LocalUser -Name $username -ErrorAction Ignore))
+       {
+          Log-Message "Creating AzDevOps user"
+          New-LocalUser -Name $username -Password $securePassword
+       }
+       else
+       {
+          Log-Message "Setting AzDevOps password"
+          Set-LocalUser -Name $username -Password $securePassword 
+       }
+
+       # Confirm the local user exists or abort if not
+       if (!(Get-LocalUser -Name $username))
+       {
+          Log-Message "Failed to create AzDevOps user"
+          exit -105
+       }
+
+       if ((Get-LocalGroup -Name "Users" -ErrorAction Ignore) -and
+           !(Get-LocalGroupMember -Group "Users" -Member $username -ErrorAction Ignore))
+       {
+          Log-Message "Adding AzDevOps to Users"
+          Add-LocalGroupMember -Group "Users" -Member $username
+       }
+       if ((Get-LocalGroup -Name "Administrators" -ErrorAction Ignore) -and
+           !(Get-LocalGroupMember -Group "Administrators" -Member $username -ErrorAction Ignore))
+       {
+          Log-Message "Adding AzDevOps to Administrators"
+          Add-LocalGroupMember -Group "Administrators" -Member $username
+       }
+       if ((Get-LocalGroup -Name "docker-users" -ErrorAction Ignore) -and
+           !(Get-LocalGroupMember -Group "docker-users" -Member $username -ErrorAction Ignore))
+       {
+          Log-Message "Adding AzDevOps to docker-users"
+          Add-LocalGroupMember -Group "docker-users" -Member $username
+       }
+
+       if ($interactive)
+       {
+          Log-Message "Configuring VM to reboot and rerun as AzDevOps to complete registration"
+          Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value $username
+          Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value $password
+          $cmd = "PowerShell.exe C:\agent\enabledagent.ps1 -url $url -pool ""$pool"" -token $token -runArgs $runArgs"
+          if ($interactiveUI)
+          {
+            $cmd += " -interactiveUI"
+          }
+
+          Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name AzDevOps -Value $cmd
+
+          Log-Message "Restarting"
+          Restart-Computer
+       }
+       else
+       {
+          Log-Message "Configuring agent to run elevated as AzDevOps without interactive UI"
+          $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $token"
+          try
+          {
+             Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
+          }
+          catch
+          {
+             Log-Message $Error[0]
+             exit -106
+          }
+
+          Log-Message "Scheduling agent to run"
+          $runCmd = Join-Path -Path $agentDir -ChildPath "run.cmd"
+          try
+          {
+             if([string]::IsNullOrEmpty($runArgs))
+             {
+                $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir
+             }
+             else
+             {
+                $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir $runArgs
+             }
+
+             $start1 = (Get-Date).AddSeconds(10)
+             $time1 = New-ScheduledTaskTrigger -At $start1 -Once 
+             Register-ScheduledTask -TaskName "PipelinesAgent" -User $username -Password $password -RunLevel Highest -Trigger $time1 -Action $cmd1 -Force
+          }
+          catch
+          {
+              Log-Message $Error[0]
+              exit -108
+          }
+       }
+    }
+    else
+    {
+       Log-Message "Configuring agent to run as Local System"
+
+       $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $token"
+       try
+       {
+          Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
+       }
+       catch
+       {
+          Log-Message $Error[0]
+          exit -107
+       }
+
+       $runCmd = Join-Path -Path $agentDir -ChildPath "run.cmd"
+       Log-Message "Scheduling agent to run"
+
+       try
+       {
+          if([string]::IsNullOrEmpty($runArgs))
+          {
+             $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir
+          }
+          else
+          {
+             $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir $runArgs
+          }
+
+          $start1 = (Get-Date).AddSeconds(10)
+          $time1 = New-ScheduledTaskTrigger -At $start1 -Once 
+          Register-ScheduledTask -TaskName "PipelinesAgent" -User System -Trigger $time1 -Action $cmd1 -Force
+       }
+       catch
+       {
+           Log-Message $Error[0]
+           exit -103
+       }
+    }
 }
 else
 {
-   Log-Message "Configuring agent to run as Local System"
+    Log-Message "Back from Reboot"
+    # Read the other registry settings
+    $url = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Url
+    $pool = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Pool
+    $token = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name Token
+    
+    # Delete the registry settings
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -ErrorAction Ignore
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -ErrorAction Ignore
 
-   $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --noRestart --replace --token $token"
-   try
-   {
-      Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
-   }
-   catch
-   {
-      Log-Message $Error[0]
-      exit -107
-   }
+    Log-Message "Configuring agent to reboot, autologon, and run unelevated as AzDevOps with interactive UI"
+    $configParameters = " --unattended --url $url --pool ""$pool"" --auth pat --replace --runAsAutoLogon --overwriteAutoLogon --windowsLogonAccount $username --windowsLogonPassword $password --token $token"
+    try
+    {
+        Start-Process -FilePath $agentConfig -ArgumentList $configParameters -NoNewWindow -Wait -WorkingDirectory $agentDir
+    }
+    catch
+    {
+        Log-Message $Error[0]
+        exit -102
+    }
 
-   $runCmd = Join-Path -Path $agentDir -ChildPath "run.cmd"
-   Log-Message "Scheduling agent to run"
-
-   try
-   {
-      if([string]::IsNullOrEmpty($runArgs))
-      {
-         $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir
-      }
-      else
-      {
-         $cmd1 = New-ScheduledTaskAction -Execute $runCmd -WorkingDirectory $agentDir $runArgs
-      }
-
-      $start1 = (Get-Date).AddSeconds(10)
-      $time1 = New-ScheduledTaskTrigger -At $start1 -Once 
-      Register-ScheduledTask -TaskName "PipelinesAgent" -User System -Trigger $time1 -Action $cmd1 -Force
-   }
-   catch
-   {
-       Log-Message $Error[0]
-       exit -103
-   }
 }
 
 Log-Message "Finished"
